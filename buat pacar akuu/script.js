c573febcb4e2fc51f925d474c60ae216eebc64ec
Ryii26 <mulyanafachry2007@gmail.com>
2024-11-08 20:54:32 +0700

    Add files via upload

---
+++ buat pacar akuu/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const celebrateButton = document.getElementById('celebrateButton');
+
+    // Menambahkan interaksi pada tombol
+    celebrateButton.addEventListener('click', () => {
+        // Ganti dengan URL halaman yang diinginkan
+        window.location.href = 'halaman_ulang_tahun.html'; 
+    });
+});
