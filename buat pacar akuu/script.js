7dc76031bdc32b0c101e4f6b15fb990353295c12
Ryii26 <mulyanafachry2007@gmail.com>
2025-01-01 16:53:55 +0700

    Delete buat pacar akuu directory

--- buat pacar akuu/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const celebrateButton = document.getElementById('celebrateButton');
-
-    // Menambahkan interaksi pada tombol
-    celebrateButton.addEventListener('click', () => {
-        // Ganti dengan URL halaman yang diinginkan
-        window.location.href = 'halaman_ulang_tahun.html'; 
-    });
-});
